45991553438b0f0644c3abe9a76add96f4921281
Ahmed Hamdy <dimo.hamdy@gmail.com>
2020-02-02 01:57:04 +0300

    Add Cats Levels of enhancement

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,32 +1,35 @@
 import Vapor
 
+enum EnhanceLevel: Int {
+    case level1
+    case level2
+    case level3
+    case level4
+    case level5
+
+}
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return temp()
+
+    router.get("level", Int.parameter) { req -> String in
+        let level = try req.parameters.next(Int.self)
+        if let level = EnhanceLevel(rawValue: level) {
+            return getData(for:  level)
+
+        } else {
+            return "Levels from 1 ... 5"
+        }
     }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
-    
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
 
-func temp() -> String{
+func getData(for level:EnhanceLevel) -> String{
     let directory = DirectoryConfig.detect()
     let configDir = "Sources/App/Assets"
     
     do {
         let fileURL = URL(fileURLWithPath: directory.workDir)
             .appendingPathComponent(configDir, isDirectory: true)
-            .appendingPathComponent("cats.json", isDirectory: false)
+            .appendingPathComponent("cats\(level.rawValue).json", isDirectory: false)
         let text2 = try String(contentsOf: fileURL, encoding: .utf8)
         return text2
     } catch {
