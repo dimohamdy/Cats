56362aeede25ab59ab951615dde36a07c9636fa7
Ahmed Hamdy <dimo.hamdy@gmail.com>
2020-02-03 01:29:42 +0300

    Support Streams with levels

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,35 +6,35 @@ enum EnhanceLevel: Int,CaseIterable {
     case level3
     case level4
     case level5
-
+    
 }
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     //EnhanceLevel.allCases.compactMap
-
-    router.get("stream") { req -> HTTPResponse in
-        
-        let directory = DirectoryConfig.detect()
-        let configDir = "Sources/App/Assets"
-
-        let fileURL = URL(fileURLWithPath: directory.workDir)
-            .appendingPathComponent(configDir, isDirectory: true)
-            .appendingPathComponent("cats1.json", isDirectory: false)
-        
-        return try req.fileio().chunkedResponse(file: fileURL.relativePath, for: req.http)
+    
+    router.get("stream", Int.parameter) { req -> HTTPResponse in
+        let level = try req.parameters.next(Int.self)
+        if let level = EnhanceLevel(rawValue: level) {
+            let fileURL =  getFilePath(for:  level)
+            return try req.fileio().chunkedResponse(file: fileURL.relativePath, for: req.http)
+        } else {
+            var res = HTTPResponse(status: .ok, body: "Levels from 1 ... 5")
+            res.contentType = .plainText
+            return res
+        }
     }
     
     router.get("level", Int.parameter) { req -> String in
         let level = try req.parameters.next(Int.self)
         if let level = EnhanceLevel(rawValue: level) {
-            return getData(for:  level)
-
+            return getData(for: getFilePath(for:  level))
+            
         } else {
             return "Levels from 1 ... 5"
         }
     }
     
-
+    
 }
 
 
@@ -42,16 +42,20 @@ public func routes(_ router: Router) throws {
 //typealias Content = String
 
 //lazy var files:[Path:Content] =
-func getData(for level:EnhanceLevel) -> String{
+func getFilePath(for level:EnhanceLevel) -> URL{
     let directory = DirectoryConfig.detect()
     let configDir = "Sources/App/Assets"
     
+    let fileURL = URL(fileURLWithPath: directory.workDir)
+        .appendingPathComponent(configDir, isDirectory: true)
+        .appendingPathComponent("cats\(level.rawValue).json", isDirectory: false)
+    return fileURL
+    
+}
+func getData(for fileURL:URL) -> String{
     do {
-        let fileURL = URL(fileURLWithPath: directory.workDir)
-            .appendingPathComponent(configDir, isDirectory: true)
-            .appendingPathComponent("cats\(level.rawValue).json", isDirectory: false)
-        let text2 = try String(contentsOf: fileURL, encoding: .utf8)
-        return text2
+        let content = try String(contentsOf: fileURL, encoding: .utf8)
+        return content
     } catch {
         print(error)
         return "Error"
