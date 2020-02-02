95307bd4d4c044f6d1a9247bb4781c730fedde47
Ahmed Hamdy <dimo.hamdy@gmail.com>
2020-02-03 00:51:38 +0300

    Add stream chunked

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,6 @@
 import Vapor
 
-enum EnhanceLevel: Int {
+enum EnhanceLevel: Int,CaseIterable {
     case level1 = 1
     case level2
     case level3
@@ -10,7 +10,20 @@ enum EnhanceLevel: Int {
 }
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
+    //EnhanceLevel.allCases.compactMap
 
+    router.get("stream") { req -> HTTPResponse in
+        
+        let directory = DirectoryConfig.detect()
+        let configDir = "Sources/App/Assets"
+
+        let fileURL = URL(fileURLWithPath: directory.workDir)
+            .appendingPathComponent(configDir, isDirectory: true)
+            .appendingPathComponent("cats1.json", isDirectory: false)
+        
+        return try req.fileio().chunkedResponse(file: fileURL.relativePath, for: req.http)
+    }
+    
     router.get("level", Int.parameter) { req -> String in
         let level = try req.parameters.next(Int.self)
         if let level = EnhanceLevel(rawValue: level) {
@@ -20,8 +33,15 @@ public func routes(_ router: Router) throws {
             return "Levels from 1 ... 5"
         }
     }
+    
+
 }
 
+
+//typealias Path = String
+//typealias Content = String
+
+//lazy var files:[Path:Content] =
 func getData(for level:EnhanceLevel) -> String{
     let directory = DirectoryConfig.detect()
     let configDir = "Sources/App/Assets"
